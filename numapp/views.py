d5434d624037f34ac16f93bba5ebd675882755d5
Yuki Oba <akihositaruto@gmail.com>
2021-07-18 23:47:43 +0900

    numapp/view.pyにgreeting.htmlをレンダリングしてレスポンスする旨を追記

--- numapp/views.py
+++ numapp/views.py
@@ -11,3 +11,14 @@ def print_msg(request):
     return render(request, 'numapp/print_msg.html', context)
 
 
+def greeting(request, pk):
+    if pk == 1:
+        grtMsg = 'おはようございます'
+    elif pk == 2:
+        grtMsg = 'こんにちは'
+    elif pk == 3:
+        grtMsg = 'こんばんは'
+    context = {
+        'greeting' : grtMsg
+    }
+    return render(request, 'numapp/greeting.html', context)
