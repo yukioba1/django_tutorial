b08a073c304a1de2fdbe5da4102b8b18a43fc3ff
Yuki Oba <akihositaruto@gmail.com>
2021-07-18 21:44:05 +0900

    numappをプロジェクトに追加
    settings.pyに 'numapp.apps.NumappConfig' を追記

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'testapp.apps.TestappConfig', 
+    'testapp.apps.TestappConfig',
+    'numapp.apps.NumappConfig',
 ]
 
 MIDDLEWARE = [
