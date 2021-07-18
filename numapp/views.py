105db395444b6f16d0d844d60040ea5ac37b5802
Yuki Oba <akihositaruto@gmail.com>
2021-07-18 23:08:42 +0900

    testapp/view.pyにprint_msg.htmlをレンダリングしてレスポンスする旨を追記

--- numapp/views.py
+++ numapp/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 def input_num(request):
     return render(request, 'numapp/input_num.html')
+
+
+def print_msg(request):
+    context = {
+        'num' : request.POST['num'],
+    }
+    return render(request, 'numapp/print_msg.html', context)
